9b14de09054daf5b6e42680801b1d3524aa609c6
SeRah <joserrh97@gmail.com>
2019-03-28 21:32:52 +0000

    eliminadas funciones invalidas

--- Tercer_Curso/UA/Practicas/Introduccion_JavaScript/Cambio-Moneda/moneda.js
+++
@@ -1,9 +0,0 @@
-var libra = document.getElementById('libra')
-var dolar = document.getElementById('dolar')
-var bolivar = document.getElementById('bolivar')
-
-function cambiarMoneda ( moneda ) {
-    libra.innerHTML = (moneda * 0.85).toFixed(2)
-    bolivar.innerHTML = (moneda * 7.86).toFixed(2)
-    dolar.innerHTML = (moneda * 1.14).toFixed(2)
-}
